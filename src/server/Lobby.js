44e3a1ea5ee74708dcc6468c3c4b174cacb23d14
kosmobot <jeppe@burchardt.com>
2014-10-03 16:12:58 +0200

    list exported controllers

--- src/server/Lobby.js
+++ src/server/Lobby.js
@@ -31,50 +31,63 @@ function Lobby (io) {
 	}
 
 	function sendLobby (socket) {
+		var controllers = [];
+		for (var s in bg.controllers) {
+			controllers.push(s);
+		}
 		socket.emit('lobby', {
 			types: ['human-vs-machine', 'machine-vs-machine'],
-			controllers: ['Aggressive', 'PrimerEndGame', 'Runner', 'Random', 'Safe']
+			controllers: controllers
 		});
 	}
 
 	function humanVsMachine (socket, options) {
-		var game = new bg.Game(3000);
-		var humanInterface = new Human(socket);
-		game.setController(humanInterface, options.playerName || 'Human');
-		game.setController(new bg.controllers[options.controllerA](), options.controllerA);
-		game.on('turnStart', function(id, dice) {
-			socket.emit('turnStart', id, dice);
-		})
-		game.on('turn', function (id, moves) {
-			socket.emit('turn', id, game.board, moves);
-		});
-		game.on('result', function (result) {
-			socket.emit('result', result);
+		try {
+			var game = new bg.Game(3000);
+			var humanInterface = new Human(socket);
+			game.setController(humanInterface, options.playerName || 'Human');
+			game.setController(new bg.controllers[options.controllerA](), options.controllerA);
+			game.on('turnStart', function(id, dice) {
+				socket.emit('turnStart', id, dice);
+			})
+			game.on('turn', function (id, moves) {
+				socket.emit('turn', id, game.board, moves);
+			});
+			game.on('result', function (result) {
+				socket.emit('result', result);
 
-		});
+			});
 
-		socket.emit('playerId', humanInterface.id);
-		socket.emit('turn', game.getCurrentIndex(), game.board);
+			socket.emit('playerId', humanInterface.id);
+			socket.emit('turn', game.getCurrentIndex(), game.board);
 
-		game.start();
+			game.start();
+		} catch (e) {
+			sendLobby(socket);
+		}
 	}
 
 	function machineVsMachine (socket, options) {
 		var game = new bg.Game(3000);
-		game.setController(new bg.controllers[options.controllerA](), options.controllerA);
-		game.setController(new bg.controllers[options.controllerB](), options.controllerB);
+		try {
+			game.setController(new bg.controllers[options.controllerA](), options.controllerA);
+			game.setController(new bg.controllers[options.controllerB](), options.controllerB);
 
-		game.on('turnStart', function(id, dice) {
-			socket.emit('turnStart', id, dice);
-		})
-		game.on('turn', function (id, moves) {
-			socket.emit('turn', id, game.board, moves);
-		});
-		game.on('result', function (result) {
-			socket.emit('result', result);
-		});
-		socket.emit('turn', game.getCurrentIndex(), game.board);
-		game.start();
+			game.on('turnStart', function(id, dice) {
+				socket.emit('turnStart', id, dice);
+			})
+			game.on('turn', function (id, moves) {
+				socket.emit('turn', id, game.board, moves);
+			});
+			game.on('result', function (result) {
+				socket.emit('result', result);
+			});
+			socket.emit('turn', game.getCurrentIndex(), game.board);
+			game.start();
+		} catch (e) {
+			console.log(bg.controllers[options.controllerA], bg.controllers, options.controllerA);
+			sendLobby(socket);
+		}
 	}
 
 
